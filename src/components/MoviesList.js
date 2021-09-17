51415db52ffa3305999a7472b7afbfdbf9fd34ad
timsch3 <timsch3@gmail.com>
2021-09-17 16:29:31 +0200

    added genre filter (only first genre)

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -5,7 +5,7 @@ import MovieCard from './MovieCard';
 class MoviesList extends React.Component {
     state = {
         moviesList: movies,
-        moviesListSorted: movies
+        genreInput: String()
     }
     sortByRating = () => {
         let tempArr = this.state.moviesList.slice()
@@ -32,15 +32,30 @@ class MoviesList extends React.Component {
         tempArr.sort((a, b) => (b.title > a.title) ? 1 : ((a.title > b.title) ? -1 : 0))
         this.setState({ moviesList: tempArr })
     }
+    showGenre = (event) => {
+        let userInput = String(event.target.value)
+        console.log(userInput)
+        if (userInput.length == 0) {
+            console.log('EMPTY')
+            this.setState({ moviesList: movies })
+        } else {
+            let moviesListCopy = movies.slice()
+            let tempArr = moviesListCopy.filter(e => e.genres[0].toLowerCase() === userInput.toLowerCase())
+            console.log(tempArr)
+            this.setState({ moviesList: tempArr })
+        }
+        this.setState({ genreInput: userInput })
+    }
     render() {
         return (
             <div>
-                <div className='moviesListButtons'>
+                <div className='moviesListInputs'>
                     <button onClick={this.sortByRating}>Sort by rating</button>
                     <button onClick={this.sortByOldest}>Sort by oldest</button>
                     <button onClick={this.sortByNewest}>Sort by newest</button>
                     <button onClick={this.sortAZ}>Sort A - Z</button>
                     <button onClick={this.sortZA}>Sort Z - A</button>
+                    Only show genre:<input type="text" value={this.state.genreInput} onChange={(event) => this.showGenre(event)} />
                 </div>
                 <div className='moviesListCards'>
                     {this.state.moviesList.map(e => <MovieCard key={e.title}
@@ -49,7 +64,7 @@ class MoviesList extends React.Component {
                         director={e.director}
                         duration={e.duration}
                         rating={e.rating}
-                        genres={e.genres.map(e => <li>{e}</li>)}
+                        genres={e.genres.map(e => <li key={e}>{e}</li>)}
                     ></MovieCard>)}
                 </div>
 
